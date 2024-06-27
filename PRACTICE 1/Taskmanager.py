314e3358e54d53255909c889a0df91ce652c8f90
ISAKA GEOREY MTWEVE <153747642+isaka-12@users.noreply.github.com>
2024-06-27 16:34:38 +0300

    Add files via upload

---
+++ PRACTICE 1/Taskmanager.py
@@ -0,0 +1,31 @@
+
+#enter option of task to be performed
+#1. Add a task to the list of tasks books to read on a day
+#2. Remove a task from the list of tasks books to read on a day
+#3. View all the tasks books to read on a day
+#4. View all the tasks books to read on a day
+option = input("Enter option of task to be performed: ")
+
+if option == "1":
+    # Add a task to the list of tasks books to read on a day
+    task = input("Enter the task to add: ")
+    # Add the task to the list
+    # Add your indented block of code here
+
+elif option == "2":
+    # Remove a task from the list of tasks books to read on a day
+    task = input("Enter the task to remove: ")
+    # Remove the task from the list
+
+elif option == "3":
+    # View all the tasks books to read on a day
+    # Print all the tasks in the list
+    # Add your indented block of code here
+
+    #elif option == "4":
+    # View all the tasks books to read on a day
+    # Print all the tasks in the list
+    # Add your indented block of code here
+
+else:
+    print("Invalid option")

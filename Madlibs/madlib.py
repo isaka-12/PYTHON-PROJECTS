4c6a33e1529c0d68493025095886d10597448f04
ISAKA GEOREY MTWEVE <153747642+isaka-12@users.noreply.github.com>
2024-07-15 02:02:59 +0300

    Create madlib.py

---
+++ Madlibs/madlib.py
@@ -0,0 +1,14 @@
+var1 = input("Enter your name: ")
+var2 = input("Enter your hobby: ")
+var3 = input("Eter your favorite programming language: ")
+var4 = input("Enter your carrier: ")
+var5 = input("Enter year you begin your carrier: ")
+var6 = input("Enter your nationality: ")
+var7 = input("Enter your native language: ")
+var8 = input("Enter your secong language: ")
+print()
+madlib = f"My name is {var1} i like {var2}. My favorite programing language is {var3}.\
+I'm a passionate {var4} since {var5}. I'm originaly a {var6} with my native language {var7}, but also\
+I can speak {var8}. "
+
+print(madlib)
